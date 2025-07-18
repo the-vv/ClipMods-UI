b68d8744a30c17f5f6553642ef906578a664e4af
Vishnu Vinod <vishnuvinod2772001@gmail.com>
2025-07-19 00:56:38 +0530

    integrate PrimeNG and Tailwind CSS with custom theme
    - Added PrimeNG button component to the app.
    - Configured PrimeNG with a custom theme using clipModsPreset.
    - Removed placeholder content from app.html and replaced it with a PrimeNG button.
    - Deleted app.spec.ts as it was no longer needed.
    - Updated app.ts to import ButtonModule from PrimeNG.
    - Modified index.html to apply a custom class for styling.
    - Enhanced global styles in styles.scss and added Tailwind CSS configuration.
    - Created clipMods.theme.ts for defining a custom theme based on Aura.
    - Added font styles in _fonts.scss for Intel One Mono and Space Mono.
    - Initialized tailwind.css for Tailwind CSS integration.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,21 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessC
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
+import { clipModsPreset } from './clipMods.theme';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: clipModsPreset
+      }
+    })
   ]
 };
