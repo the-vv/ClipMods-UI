348d2ef6855e58ae88b98d81774e49b6c2bc72f2
Vishnu Vinod <vishnuvinod2772001@gmail.com>
2025-08-26 22:51:58 +0530

    Implemented loading state management with a progress bar and updated router event handling for loading indication.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,20 +1,22 @@
 import { AfterViewInit, Component, ElementRef, inject, OnInit, signal, viewChild } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router, RouterOutlet } from '@angular/router';
 import { ModEngine } from './services/mod-engine';
 import { BlockUIModule } from 'primeng/blockui';
 import { CommonService } from './services/common-service';
 import { AsyncPipe } from '@angular/common';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { ProgressBarModule } from 'primeng/progressbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, BlockUIModule, AsyncPipe],
+  imports: [RouterOutlet, BlockUIModule, AsyncPipe, ProgressBarModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App implements AfterViewInit {
   protected readonly modEngineService = inject(ModEngine);
   protected readonly commonService = inject(CommonService);
+  private readonly router = inject(Router);
   iframeRef = viewChild.required<ElementRef<HTMLIFrameElement>>('sandbox');
   private modLoadingTexts = [
     "Unleashing the mod...",
@@ -29,6 +31,7 @@ export class App implements AfterViewInit {
     "The mod is racing through functions..."
   ];
   currentLoadingText = signal('');
+  isLoadingPage = signal(false);
   constructor() {
     this.modEngineService.modIsRunning$.pipe(takeUntilDestroyed()).subscribe(isRunning => {
       if (isRunning) {
@@ -37,7 +40,14 @@ export class App implements AfterViewInit {
       } else {
         this.currentLoadingText.set('Please Wait...');
       }
-    })
+    });
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationStart) {
+        this.isLoadingPage.set(true);
+      } else if (event instanceof NavigationEnd || event instanceof NavigationCancel || event instanceof NavigationError) {
+        this.isLoadingPage.set(false);
+      }
+    });
   }
 
 
